8ebbb705fd58e7d272bc4f0d2e132d288e800cea
Bossman8 <jones.boston8@gmail.com>
2019-07-30 00:24:16 -0600

    added files to repo

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBLMCVUOqJJKLoDB4-KXsodz_6UsfTE7zA",
+    authDomain: "rockpaperscissors-3bb3a.firebaseapp.com",
+    databaseURL: "https://rockpaperscissors-3bb3a.firebaseio.com",
+    projectId: "rockpaperscissors-3bb3a",
+    storageBucket: "",
+    messagingSenderId: "352487381913",
+    appId: "1:352487381913:web:82fb42b55f303a71"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
