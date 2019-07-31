2d0b5f00eaa03cc154d472de5d7678c69d06643c
Bossman8 <jones.boston8@gmail.com>
2019-07-30 18:02:06 -0600

    updated css js and html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,9 +3,37 @@ var firebaseConfig = {
     authDomain: "rockpaperscissors-3bb3a.firebaseapp.com",
     databaseURL: "https://rockpaperscissors-3bb3a.firebaseio.com",
     projectId: "rockpaperscissors-3bb3a",
-    storageBucket: "",
+    storageBucket: "rockpaperscissors-3bb3a.appspot.com",
     messagingSenderId: "352487381913",
-    appId: "1:352487381913:web:82fb42b55f303a71"
-};
+    appId: "1:352487381913:web:26df983973e6f95a"
+  };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+
+firebase.database().on("value",function(snapshot){
+      console.log(snapshot.val());
+  })
+
+
+p1name = "";
+p2name = "";
+p1score = 0;
+p2score = 0;
+
+$("#p1score").text(p1score)
+$("#p2score").text(p2score)
+
+$("#addUser").on("click", function() {
+    p1name = $("#userName").val().trim();
+    // if (p1name.length === 0) {
+    //     p1name = $("#userName").val().trim();
+    // }
+    // else if (p2name.length === 0) {
+    //     p2name = $("#userName").val().trim();
+    // }
+    firebase.database().ref().set({
+        p1name: p1name,
+        p2name: p2name,
+    })
+})
